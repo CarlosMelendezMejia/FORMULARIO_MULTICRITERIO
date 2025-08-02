c856c11b0892545f6b73eb8847699e709419972e
CarlosMelendezMejia <72164372+CarlosMelendezMejia@users.noreply.github.com>
2025-08-02 00:57:37 -0600

    Configure DB via environment variables

--- db.py
+++ db.py
@@ -1,11 +1,13 @@
+import os
 import mysql.connector
 
 def get_connection():
+    """Create a connection to the MySQL database using environment variables."""
     return mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='wavedlizard2115',
-        database='sistema_formularios'
+        host=os.environ.get("DB_HOST"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME"),
     )
 
 def get_cursor(conn):
